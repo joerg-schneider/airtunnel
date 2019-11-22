4c4e8bf1295ef7b519cdbde1512ed576f4fd8856
joerg-schneider <joerg@joerg-schneider.org>
2019-11-22 22:11:07 +0100

    add docstrings for data_store

--- src/airtunnel/data_store.py
+++ src/airtunnel/data_store.py
@@ -1,3 +1,4 @@
+""" Module for Airtunnel DataStore abstractions. """
 import glob
 import importlib
 import logging
@@ -17,64 +18,80 @@ DEFAULT_ADAPTER_CLASS = "airtunnel.data_store.LocalDataStoreAdapter"
 
 
 class BaseDataStoreAdapter(ABC):
+    """ Base class for all Airtunnel DataStoreAdapters. """
+
     @staticmethod
     @abstractmethod
     def move(source: str, destination: str, recursive: bool = False) -> None:
+        """ Move file(s) from source to destination. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def copy(source: str, destination: str, recursive: bool = False) -> None:
+        """ Copy file(s) from source to destination. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def delete(path: str, recursive: bool = False) -> None:
+        """ Delete file(s) at a given path. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def makedirs(path: str, exist_ok: bool = False, **kwargs) -> None:
+        """ Make directories along the given path. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def open(file: str, mode: str, **kwargs) -> TextIO:
+        """ Open a file-handle with the given path & mode. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def exists(path: str, **kwargs) -> bool:
+        """ Checks the given path exists. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def glob(pattern: str, **kwargs) -> List[str]:
+        """ Fetch file list using the provided glob pattern. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def listdir(path: str, **kwargs) -> List[str]:
+        """ List entries of a given path. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def modification_times(files: List[str]) -> Dict[str, int]:
+        """ For a given file list, fetch modification times as int Unix timestamps. """
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def inspect(files: List[str]) -> Dict[str, Tuple[datetime, datetime, int]]:
+        """ For a given file list, fetch create & modification datetime and byte size. """
         raise NotImplementedError
 
 
 class LocalDataStoreAdapter(BaseDataStoreAdapter):
+    """ DataStoreAdapter implementation targeting the local filesystem. """
+
     @staticmethod
     def move(source: str, destination: str, recursive: bool = False) -> None:
+        """ Move file(s) from source to destination. """
         shutil.move(src=source, dst=destination)
 
     @staticmethod
     def copy(source: str, destination: str, recursive: bool = False) -> None:
+        """ Copy file(s) from source to destination. """
         if recursive:
             shutil.copytree(src=source, dst=destination)
         else:
@@ -82,6 +99,7 @@ class LocalDataStoreAdapter(BaseDataStoreAdapter):
 
     @staticmethod
     def delete(path: str, recursive: bool = False) -> None:
+        """ Delete file(s) at a given path. """
         if recursive:
             shutil.rmtree(path=path)
         else:
@@ -89,31 +107,38 @@ class LocalDataStoreAdapter(BaseDataStoreAdapter):
 
     @staticmethod
     def makedirs(path: str, exist_ok: bool = False, **kwargs) -> None:
+        """ Make directories along the given path. """
         os.makedirs(path, exist_ok=exist_ok, **kwargs)
 
     @staticmethod
     def open(file: str, mode: str, **kwargs) -> TextIO:
+        """ Open a file-handle with the given path & mode. """
         return open(file=file, mode=mode, **kwargs)
 
     @staticmethod
     def exists(path: str, **kwargs) -> bool:
+        """ Checks the given path exists. """
         return os.path.exists(path=path)
 
     @staticmethod
     def glob(pattern: str, **kwargs) -> List[str]:
+        """ Fetch file list using the provided glob pattern. """
         return glob.glob(pattern)
 
     @staticmethod
     def listdir(path: str, **kwargs) -> List[str]:
+        """ List entries of a given path. """
         return os.listdir(path)
 
     @staticmethod
     def modification_times(files: List[str]) -> Dict[str, int]:
+        """ For a given file list, fetch modification times as int Unix timestamps. """
         return {f: os.stat(f).st_mtime for f in files}
 
     @staticmethod
     @abstractmethod
     def inspect(files: List[str]) -> Dict[str, Tuple[datetime, datetime, int]]:
+        """ For a given file list, fetch create & modification datetime and byte size. """
         inspected = {}
         for f in files:
             f_stat = os.stat(f)
@@ -127,14 +152,19 @@ class LocalDataStoreAdapter(BaseDataStoreAdapter):
 
 
 def get_configured_adapter() -> BaseDataStoreAdapter:
+    """ Gets the configured DataStoreAdapter. """
     try:
         data_store_adapter_class = conf.get(
             section="airtunnel", key="data_store_adapter_class"
         )
     except AirflowConfigException:
-        logger.warning(f"'data_store_adapter_class' for Airtunnel not configured in airflow.cfg – using default")
+        logger.warning(
+            f"'data_store_adapter_class' for Airtunnel not configured in airflow.cfg – using default"
+        )
         # set the default config as part of the environment, to hide future AirflowConfigExceptions:
-        os.environ["AIRFLOW__AIRTUNNEL__DATA_STORE_ADAPTER_CLASS"] = DEFAULT_ADAPTER_CLASS
+        os.environ[
+            "AIRFLOW__AIRTUNNEL__DATA_STORE_ADAPTER_CLASS"
+        ] = DEFAULT_ADAPTER_CLASS
         data_store_adapter_class = DEFAULT_ADAPTER_CLASS
 
     module, cls = data_store_adapter_class.rsplit(".", maxsplit=1)
